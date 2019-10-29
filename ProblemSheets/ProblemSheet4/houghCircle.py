d20b5e5e28f27694e995abce27fc9d11f30509e7
dahutchinson <dom@domhutchinson.me>
2019-10-29 14:34:40 +0000

    Better circles

--- ProblemSheets/ProblemSheet4/houghCircle.py
+++ ProblemSheets/ProblemSheet4/houghCircle.py
@@ -1,10 +1,7 @@
 import numpy as np
 import cv2 as cv
 import sys
-
-colour=cv.imread("img/coins1.png",1)
-magnitude=cv.imread("img/magnitude.png",cv.IMREAD_GRAYSCALE)
-direction=cv.imread("img/direction.png",cv.IMREAD_GRAYSCALE)
+import sobel
 
 def thresholding(mag:np.ndarray,dir:np.ndarray,threshold=64) -> "np.ndarray,np.ndarray":
     mag_thres=np.zeros(mag.shape); dir_thres=np.zeros(dir.shape)
@@ -12,7 +9,7 @@ def thresholding(mag:np.ndarray,dir:np.ndarray,threshold=64) -> "np.ndarray,np.n
         for j in range(0,mag.shape[1]):
             if (mag[i,j]>threshold):
                 mag_thres[i,j]=255
-                dir_thres[i,j]=direction[i,j]
+                dir_thres[i,j]=dir[i,j]
             else:
                 dir_thres[i,j]=128
                 mag_thres[i,j]=0
@@ -26,9 +23,8 @@ def hough(mag:np.ndarray,dir:np.ndarray,hough_threshold=100,min_radius=10,max_ra
             if mag[i,j]==255:
                 # increment all points on line of possible radia
                 for r in range(min_radius,max_radius+1):
-                    psi=2*np.pi*(dir[i,j]/255)-np.pi
-                    y_c=i-int(r*np.sin(psi))
-                    x_c=j+int(r*np.cos(psi))
+                    y_c=i-int(r*np.sin(dir[i,j]))
+                    x_c=j+int(r*np.cos(dir[i,j]))
                     if (y_c>=0) and (y_c<parameter_space.shape[0]) and (x_c>=0) and (x_c<parameter_space.shape[1]):
                         parameter_space[y_c,x_c,r]+=1
     # implement thresholding
@@ -68,24 +64,69 @@ def hough_img(colour_img:np.ndarray,hough_space:np.ndarray,threshold=5):
             for k in range(0,hough_space.shape[2]):
                 if (hough_space[i,j,k]>=threshold):
                     overlay[i,j]=[0,0,255]
-                    to_plot.append([i,j,k])
+                    to_plot.append([hough_space[i,j,k],i,j,k])
                     centres[i,j]=255
                 else:
                     overlay[i,j]=colour_img[i,j]
-    for (i,j,k) in to_plot:
-        cv.circle(overlay, (j,i), k, (0,0,255), 2)
+
+    to_plot.sort(key=lambda x:x[0], reverse=True)
+    print(len(to_plot))
+    plotted=[]
+    for (s,i,j,k) in to_plot:
+        overlap=False
+
+        # check whether this circle overlaps with any that have already been plotted
+        for (i0,j0,k0) in plotted:
+            distSq=(i0-i)**2 + (j0-j)**2
+            radSumSq=(k0+k)**2
+
+            if distSq<radSumSq:
+                overlap=True
+                break
+
+        if (not overlap):
+            cv.circle(overlay, (j,i), k, (0,0,255), 2)
+            plotted.append([i,j,k])
 
     return overlay, centres
 
+def run(path):
+    img=cv.imread(path,0)
+    colour_img=cv.imread(path,1)
+    dx=np.matrix([[-1,0,1],[-2,0,2],[-1,0,1]])
+    dy=np.matrix([[1,2,1],[0,0,0],[-1,-2,-1]])
 
-magnitude_threshold,direction_threshold=thresholding(magnitude,direction,threshold=64)
-cv.imwrite("img/magnitude_threshold.png",magnitude_threshold)
-cv.imwrite("img/direction_threshold.png",direction_threshold)
+    img_dx=sobel.convolution(img,dx)
+    cv.imwrite("img/dx.png",img_dx)
+    print("DX done")
+    img_dy=sobel.convolution(img,dy)
+    cv.imwrite("img/dy.png",img_dy)
+    print("DY done")
 
-hough_space=hough(magnitude_threshold,direction_threshold,min_radius=20,max_radius=100)
-hough_image=hough_magnitude(hough_space,threshold=0)
-cv.imwrite("img/hough_img.png",hough_image)
+    magnitude=sobel.magnitude(img_dx,img_dy)
+    print("Magnitude done")
+    magnitude=sobel.normalise(magnitude)
+    cv.imwrite("img/magnitude.png",magnitude)
+    print("Magnitude normalised")
+    direction=sobel.direction(img_dx,img_dy)
+    direction_normalised=sobel.normalise(direction,min=-np.pi,max=np.pi)
+    cv.imwrite("img/direction.png",direction_normalised)
+    print("Direction done")
 
-overlay,centres=hough_img(colour,hough_space,threshold=10)
-cv.imwrite("img/centres.png",centres)
-cv.imwrite("img/overlay.png",overlay)
+    magnitude_threshold,direction_threshold=thresholding(magnitude,direction_normalised,threshold=150)
+    print("thresholded")
+    cv.imwrite("img/magnitude_threshold.png",magnitude_threshold)
+    cv.imwrite("img/direction_threshold.png",direction_threshold)
+
+    hough_space=hough(magnitude_threshold,direction,min_radius=30,max_radius=60)
+    print("hough done")
+    hough_image=hough_magnitude(hough_space,threshold=0)
+    print("hough image")
+    cv.imwrite("img/hough_img.png",hough_image)
+
+    overlay,centres=hough_img(colour_img,hough_space,threshold=10)
+    print("overlay done")
+    cv.imwrite("img/centres.png",centres)
+    cv.imwrite("img/overlay.png",overlay)
+
+run("img/coins2.png")
--- ProblemSheets/ProblemSheet4/sobel.py
+++ ProblemSheets/ProblemSheet4/sobel.py
@@ -43,16 +43,17 @@ def normalise(img:[[]],min=None,max=None) -> [[]]:
     for i in range(0,img.shape[0]):
         for j in range(0,img.shape[1]):
             normalised[i,j]=int(((img[i,j]-min)/(max-min))*255)
+
     return normalised
 
 def magnitude(dx:[[]],dy:[[]]) -> [[]]:
-    m=np.zeros(img.shape)
+    m=np.zeros(dx.shape)
 
     for i in range(0,m.shape[0]):
         for j in range(0,m.shape[1]):
             m[i,j]=np.sqrt(dx[i,j]**2 + dy[i,j]**2)
 
-    return normalise(m)
+    return m
 
 def direction(dx:[[]],dy:[[]]) -> [[]]:
     d=np.zeros(dx.shape)
@@ -61,28 +62,30 @@ def direction(dx:[[]],dy:[[]]) -> [[]]:
             if dx[i,j]==0:
                 dx[i,j]=1/sys.maxsize
             d[i,j]=np.arctan2(dy[i,j],dx[i,j])
-    normalised=normalise(d,min=-np.pi,max=np.pi)
-    return normalised
+    return d
 
-dx=np.matrix([[-1,0,1],[-2,0,2],[-1,0,1]])
-dy=np.matrix([[1,2,1],[0,0,0],[-1,-2,-1]])
+def run():
+    dx=np.matrix([[-1,0,1],[-2,0,2],[-1,0,1]])
+    dy=np.matrix([[1,2,1],[0,0,0],[-1,-2,-1]])
 
-img=cv.imread("img/coins1.png",cv.IMREAD_GRAYSCALE)
+    img=cv.imread("img/coins1.png",cv.IMREAD_GRAYSCALE)
 
-# dx gradient
-convolved_dx=convolution(img,dx)
-normalised_dx=normalise(convolved_dx)
-cv.imwrite("img/dx.png",normalised_dx)
+    # dx gradient
+    convolved_dx=convolution(img,dx)
+    normalised_dx=normalise(convolved_dx)
+    cv.imwrite("img/dx.png",normalised_dx)
 
-# dy gradient
-convolved_dy=convolution(img,dy)
-normalised_dy=normalise(convolved_dy)
-cv.imwrite("img/dy.png",normalised_dy)
+    # dy gradient
+    convolved_dy=convolution(img,dy)
+    normalised_dy=normalise(convolved_dy)
+    cv.imwrite("img/dy.png",normalised_dy)
 
-# magnitude
-normalised_mag=magnitude(convolved_dx,convolved_dy)
-cv.imwrite("img/magnitude.png",normalised_mag)
+    # magnitude
+    mag=magnitude(convolved_dx,convolved_dy)
+    normalised_mag=normalise(mag)
+    cv.imwrite("img/magnitude.png",normalised_mag)
 
-# direction
-normalised_dir=direction(convolved_dx,convolved_dy)
-cv.imwrite("img/direction.png",normalised_dir)
+    # direction
+    dir=direction(convolved_dx,convolved_dy)
+    normalised_dir=normalise(dir,min=-np.pi,max=np.pi)
+    cv.imwrite("img/direction.png",normalised_dir)
